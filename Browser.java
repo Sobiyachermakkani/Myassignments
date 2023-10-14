4c619ce4ed923580d647f161671ffac2ecdb4375
Sobiyachermakkani <53512496+Sobiyachermakkani@users.noreply.github.com>
2023-10-14 21:22:41 +0530

    Add files via upload
    Week 1_Day 1_Homework assignments

---
+++ Browser.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserVersion=8.7f;
+		String browserName="chrome";
+		boolean  pageLoaded= true;
+		int noOfBrowser=3;
+		char browserLogo='S';
+		System.out.println(browserVersion +" "+browserName+" "+pageLoaded+" "+noOfBrowser+" "+browserLogo);
+
+	}
+
+}
---
+++ FindNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int a=45;
+		if(a>0) {
+			System.out.println("The number is Positive");
+		}
+		else
+		{
+			System.out.println("The number is Negative");	
+		}
+	}
+
+}
